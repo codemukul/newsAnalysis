552ada04edaaeaaaf762bb2f577534ec31a55bc7
Mukul Kumar Gupta <76431593+codemukul@users.noreply.github.com>
2023-08-12 13:47:04 +0000

    feat: implemented the meaningcloud API for NLP

---
+++ src/client/js/requests.js
@@ -0,0 +1,32 @@
+const getData = async () => {
+  const res = await fetch('http://localhost:8081/get_analysis');
+  try {
+    const data = await res.json();
+    if (!data.message) Client.displayWarn();
+    console.log(data);
+    Client.updateUI(data);
+  } catch (err) {
+    console.log(err.message);
+  }
+};
+
+
+const postInputValue = async (data, URL) => {
+    const res = await fetch(URL, {
+        method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      body: JSON.stringify(data),
+    });
+    try {
+      const data = await res.json();
+      return data;
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+export { postInputValue, getData };
--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,83 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
+var path = require('path');
+const express = require('express');
+// const mockAPIResponse = require('./mockAPI.js')
 
-const app = express()
+const app = express();
 
-app.use(express.static('dist'))
+// body-parser middleware
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-console.log(__dirname)
+// Cross-origin
+const cors = require('cors');
+const { stringify } = require('querystring');
+app.use(cors());
+
+app.use(express.static('dist'));
+
+console.log(__dirname);
+
+var textapi = {
+    application_key: process.env.API_KEY,
+};
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-})
+});
 
-// designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
-})
+});
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
+
+let input_data;
+
+const analysis_data = {
+};
+
+const api_endpoint = (input_data) => {
+    return `https://api.meaningcloud.com/sentiment-2.1?key=${textapi.application_key}&lang=auto&${input_data.type}=${input_data.value}`;
+};
+  
+const fetch_api = async (URL) => {
+  const res = await fetch(URL, {
+    method: 'POST',
+    redirect: 'follow',
+  });
+  try {
+    const data = await res.json();
+    if (data.status.msg === 'OK') {
+      analysis_data.message = true;
+      analysis_data.text = data.sentence_list[0].text;
+      analysis_data.subjectivity = data.subjectivity;
+      analysis_data.confidence = data.confidence;
+      analysis_data.polarity = data.score_tag;
+    } else {
+      analysis_data.message = false;
+      analysis_data.text = '';
+      analysis_data.subjectivity = '';
+      analysis_data.confidence = '';
+      analysis_data.polarity = '';
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+  
+app.post('/send_sample', (req, res) => {
+    const data = req.body;
+    input_data = data;
+});
+  
+app.get('/get_analysis', function (req, res) {
+  const URL = api_endpoint(input_data);
+  fetch_api(URL).then(() => {
+    res.send(analysis_data);
+  });
+});
